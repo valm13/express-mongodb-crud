0b4efd7733fc24548ff2522bb8eddd591d6074e7
Eric Muellenbach <emuellenbach@gmx.de>
2020-04-20 14:12:00 +0200

    Add Dockerization

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // connection to db
-mongoose.connect('mongodb://localhost/crud-mongo')
+mongoose.connect('mongodb://mongodb/crud-mongo')
   .then(db => console.log('db connected'))
   .catch(err => console.log(err));
 
